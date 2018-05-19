7b09735ce153ad92c9f8a0c6cebd8c8e7962aa3b
Nathan Worsham <nathan.worsham@pinnacol.com>
2018-05-18 23:26:29 -0600

    first iteration of counts dashboard

---
+++ appserver/static/tabledatabar.js
@@ -0,0 +1,21 @@
+require([
+    'jquery',
+    'underscore',
+    'splunkjs/mvc',
+    'views/shared/results_table/renderers/BaseCellRenderer',
+    'splunkjs/mvc/simplexml/ready!'
+], function($, _, mvc, BaseCellRenderer) {
+    var DataBarCellRenderer = BaseCellRenderer.extend({
+        canRender: function(cell) {
+            return (cell.field === 'percent');
+        },
+        render: function($td, cell) {
+            $td.addClass('data-bar-cell').html(_.template('<div class="data-bar-wrapper"><div class="data-bar" style="width:<%- percent %>%"></div></div>', {
+                percent: Math.min(Math.max(parseFloat(cell.value), 0), 100)
+            }));
+        }
+    });
+    mvc.Components.get('tabledatabar').getVisualization(function(tableView) {
+        tableView.addCellRenderer(new DataBarCellRenderer());
+    });
+});
