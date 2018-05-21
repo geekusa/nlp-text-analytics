fbd0d5e63378f87634d88f811cbb561f69a23366
Nathan Worsham <nathan.worsham@pinnacol.com>
2018-05-21 14:19:48 -0600

    added appIcons and incomplete code for slider

--- appserver/static/tabledatabar.js
+++ appserver/static/tabledatabar.js
@@ -5,6 +5,11 @@ require([
     'views/shared/results_table/renderers/BaseCellRenderer',
     'splunkjs/mvc/simplexml/ready!'
 ], function($, _, mvc, BaseCellRenderer) {
+    //change text input into range slider (not completely working correctly yet)
+    $("[id^=range]").find("input")
+                .attr('type','range')
+                .attr('min','25')
+               .attr('max','400')
     var DataBarCellRenderer = BaseCellRenderer.extend({
         canRender: function(cell) {
             return (cell.field === 'proportion');
