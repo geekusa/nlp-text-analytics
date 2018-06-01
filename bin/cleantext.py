552d4b2b96c224e6a63ae412f2d3a17e7a656744
Nathan Worsham <nathan.worsham@pinnacol.com>
2018-06-01 11:42:08 -0600

    fix local streaming again

--- bin/cleantext.py
+++ bin/cleantext.py
@@ -21,7 +21,7 @@ CORPORA_DIR = os.path.join(BASE_DIR,'bin','nltk_data')
 nltk_data_path.append(CORPORA_DIR)
 
 
-@Configuration(streaming=True, local=True)
+@Configuration(local=True)
 class CleanText(StreamingCommand):
     """ Counts the number of non-overlapping matches to a regular expression in a set of fields.
 
