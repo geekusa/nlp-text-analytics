0d4b54140eb0a8062e0975e04d2276fba8643513
geekusa <nworsham@gmail.com>
2020-02-07 22:13:07 -0700

    update vader for python 3 compatibility

--- bin/vader.py
+++ bin/vader.py
@@ -9,21 +9,13 @@ Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
 
 import sys
 import os
-import logging, logging.handlers
 
-#from string import punctuation, digits, maketrans
-
-from splunk import setupSplunkLogger
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.data import path as nltk_data_path
 from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
 
-try:
-    from splunk.clilib.bundle_paths import make_splunkhome_path
-except ImportError:
-    from splunkappserver.mrsparkle.lib.util import make_splunkhome_path
-
-BASE_DIR = make_splunkhome_path(["etc","apps","nlp-text-analytics"])
+splunkhome = os.environ['SPLUNK_HOME']
+BASE_DIR = os.path.join(splunkhome, 'etc', 'apps', 'nlp-text-analytics')
 CORPORA_DIR = os.path.join(BASE_DIR,'bin','nltk_data')
 nltk_data_path.append(CORPORA_DIR)
 
@@ -64,33 +56,6 @@ class Vader(StreamingCommand):
         **Syntax:** **full_output=***<fieldname>*
         **Description:** If true, returns full sentiment values--neutral, positive, and negative--otherwise only compound is reutrned''',
         validate=validators.Boolean())
-
-    #http://dev.splunk.com/view/logging/SP-CAAAFCN
-    def setup_logging(self):
-        logger = logging.getLogger('splunk.foo')    
-        SPLUNK_HOME = os.environ['SPLUNK_HOME']
-        
-        LOGGING_DEFAULT_CONFIG_FILE = os.path.join(SPLUNK_HOME, 'etc', 'log.cfg')
-        LOGGING_LOCAL_CONFIG_FILE = os.path.join(SPLUNK_HOME, 'etc', 'log-local.cfg')
-        LOGGING_STANZA_NAME = 'python'
-        LOGGING_FILE_NAME = "nlp-text-analytics.log"
-        BASE_LOG_PATH = os.path.join('var', 'log', 'splunk')
-        LOGGING_FORMAT = "%(asctime)s %(levelname)-s\t%(module)s:%(lineno)d - %(message)s"
-        splunk_log_handler = logging.handlers.RotatingFileHandler(
-            os.path.join(
-                SPLUNK_HOME,
-                BASE_LOG_PATH,
-                LOGGING_FILE_NAME
-            ), mode='a') 
-        splunk_log_handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
-        logger.addHandler(splunk_log_handler)
-        setupSplunkLogger(
-            logger,
-            LOGGING_DEFAULT_CONFIG_FILE,
-            LOGGING_LOCAL_CONFIG_FILE,
-            LOGGING_STANZA_NAME
-        )
-        return logger
 
     def stream(self, records):
         sentiment_analyzer = SentimentIntensityAnalyzer()
